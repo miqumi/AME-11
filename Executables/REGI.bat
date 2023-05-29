34440cb309b814b940d16b873c1689641a65cf62
Shrey <80510430+miqumi@users.noreply.github.com>
2023-05-29 12:20:06 +0530

    remove some stuff
    removed firefox injection folder:
    extra space!
    stopped ff config stuff
    no fileassoc
    fileassoc bad
    do not install scoop apps
    remove nomacs config (unecessary)
    remove more stuff
    check commit log to know more changes
    
    Co-Authored-By: SaturnEimi <118990775+saturneimi@users.noreply.github.com>
    Co-Authored-By: SwareetKherde1 <103266430+swareetkherde1@users.noreply.github.com>

--- Executables/REGI.bat
+++ Executables/REGI.bat
@@ -10,18 +10,9 @@ for /f "usebackq delims=" %%A in (`reg query "HKCR" /k /f "AppX" ^| findstr /c:"
 )
 @echo on
 
-:: smb config
-sc config lanmanworkstation depend= bowser/mrxsmb20/nsi
-
-:: Enable Legacy F8 Bootmenu
-bcdedit /set {default} bootmenupolicy legacy
-:: Disable Recovery
-bcdedit /set {current} recoveryenabled no
-
 :: Disable Hibernation to make NTFS accessable outside of Windows
 powercfg /h off
 :: Set Performance Plan to High Performance and display to never turn off
 powercfg /S 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-powercfg /change monitor-timeout-ac 0
 
 copy /y "UsrClass.dat" "%SYSTEMDRIVE%\Users\Default\AppData\Local\Microsoft\Windows"
--- Executables/SHRT.bat
+++ Executables/SHRT.bat
@@ -6,15 +6,6 @@ copy /y "ameck.exe" "%WINDIR%\System32"
 for /f "usebackq delims=" %%A in (`dir /b /a:d "%SYSTEMDRIVE%\Users" ^| findstr /v /i /x /c:"Public" /c:"Default User" /c:"All Users"`) do (
 	echo mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell"
 	mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell"
-	echo mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned"
-	mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned"
-
-	echo mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nomacs"
-	mkdir "%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nomacs"
-
-	echo PowerShell -NoP -C "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Terminal.lnk'); $S.TargetPath = '%PROGRAMDATA%\Scoop\GlobalScoopApps\apps\windows-terminal\current\WindowsTerminal.exe'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Thunderbird.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\Mozilla Thunderbird\thunderbird.exe'; $S.WorkingDirectory = 'C:\Program Files\Mozilla Thunderbird'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Firefox.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\Mozilla Firefox\firefox.exe'; $S.WorkingDirectory = 'C:\Program Files\Mozilla Firefox'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nomacs\nomacs - Image Lounge.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\nomacs\bin\nomacs.exe'; $S.WorkingDirectory = 'C:\Program Files\nomacs; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Central AME Script.lnk'); $S.TargetPath = '%WINDIR%\System32\amecs.exe'; $S.Save()"
-	PowerShell -NoP -C "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Terminal.lnk'); $S.TargetPath = '%PROGRAMDATA%\Scoop\GlobalScoopApps\apps\windows-terminal\current\WindowsTerminal.exe'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Thunderbird.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\Mozilla Thunderbird\thunderbird.exe'; $S.WorkingDirectory = 'C:\Program Files\Mozilla Thunderbird'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Firefox.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\Mozilla Firefox\firefox.exe'; $S.WorkingDirectory = 'C:\Program Files\Mozilla Firefox'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nomacs\nomacs - Image Lounge.lnk'); $S.TargetPath = '%HOMEDRIVE%\Program Files\nomacs\bin\nomacs.exe'; $S.WorkingDirectory = 'C:\Program Files\nomacs'; $S.Save(); $s = $ws.CreateShortcut('%SYSTEMDRIVE%\Users\%%A\AppData\Roaming\OpenShell\Pinned\Central AME Script.lnk'); $S.TargetPath = '%WINDIR%\System32\amecs.exe'; $S.Save()"
-)
 
 @echo ON
 
